597a73f31454c1b4d8576b27a93ff5e9dd7a4199
Yan <podoyo23@gmail.com>
2025-04-23 14:51:35 +0800

    add topic

---
+++ 練習題1：找出成績最高的學生/bestScoreStudent.py
@@ -0,0 +1,28 @@
+"""
+練習題1：找出成績最高的學生
+請你寫一段程式：
+
+- 使用 .keys() 取得所有的 key（學生名字）。
+
+- 找出「分數最高的學生」是誰。
+
+- 印出該學生的名字與分數。                                                                           ✅ 範例輸出格式：
+"""
+
+# 輸出結果是: 最高分是 Alice，分數是 95分
+
+my_dict_stu = {
+"Tom": 85,
+"Alice": 95,
+"Bob": 78,
+"John": 60,
+"Lucy": 88,
+"Ken": 92,
+"Emma": 74
+}
+
+All_keys = my_dict_stu.keys()
+# print(All_keys)
+# ['Tom', 'Alice', 'Bob', 'John', 'Lucy', 'Ken', 'Emma']
+top_score = 0 # 先設定為0
+top_score_name = "" # 先設定為空字串
