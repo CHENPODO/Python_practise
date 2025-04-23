77d3255adf4fe9827166980f7770b76cefc51489
Yan <podoyo23@gmail.com>
2025-04-23 15:01:22 +0800

    update:topic2 answer

--- 練習題2：找出最貴的商品/expensiveProductPrice.py
+++ 練習題2：找出最貴的商品/expensiveProductPrice.py
@@ -11,3 +11,11 @@ product_prices = {
     "滑鼠": 1200,
     "螢幕": 7800
 }
+expensive_produce_price = 0 # 先定為0
+expensive_produce_name = "" # 空字串候傳入
+
+for price in product_prices.keys():
+    if product_prices[price] > expensive_produce_price:
+        expensive_produce_name = price # 取得之key值
+        expensive_produce_price = product_prices[price]
+print(f"最貴的商品是:{expensive_produce_name},價格是:{expensive_produce_price}")
