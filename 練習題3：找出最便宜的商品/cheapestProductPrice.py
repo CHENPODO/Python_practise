9e63d62a5358fa3d956ed5d9e10cc7376d66edc0
Yan <podoyo23@gmail.com>
2025-04-23 15:05:40 +0800

    update:topic3 answer

--- 練習題3：找出最便宜的商品/cheapestProductPrice.py
+++ 練習題3：找出最便宜的商品/cheapestProductPrice.py
@@ -10,4 +10,12 @@ product_prices = {
     "滑鼠": 1200,
     "螢幕": 7800
 }
+cheapest_produce_price = 9999999999 # 先定為0
+cheapest_produce_name = "" # 空字串候傳入
+
+for price in product_prices.keys():
+    if product_prices[price] < cheapest_produce_price:
+        cheapest_produce_name = price # 取得之key值
+        cheapest_produce_price = product_prices[price]
+print(f"最便宜的商品是:{cheapest_produce_name},價格是:{cheapest_produce_price}")
 
