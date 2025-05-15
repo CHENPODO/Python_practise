7caa9a372ef9cb80e4ef2b4b9872403af0cb7ab4
Yan <podoyo23@gmail.com>
2025-05-15 14:29:49 +0800

    add chapter11 -Map import folium

---
+++ 教學影片練習/Chapter11/01_可視畫地圖.py
@@ -0,0 +1,94 @@
+import folium
+from folium import FeatureGroup
+
+"""
+| 景點名稱    | 緯度（Lat）   | 經度（Lng）    |
+| ------- | --------- | ---------- |
+| 礁溪溫泉公園  | 24.827657 | 121.772424 |
+| 湯圍溝溫泉公園 | 24.829552 | 121.773001 |
+| 礁溪火車站   | 24.827020 | 121.774690 |
+| 五峰旗瀑布   | 24.842203 | 121.788514 |
+| 礁溪國小    | 24.821977 | 121.768499 |
+
+"""
+
+# 建立礁溪鄉地圖
+jiaoxi_map = folium.Map(
+    location=[24.8261,121.7728], # 礁溪鄉中心點
+    tiles="openstreetmap",
+    zoom_start=16
+)
+
+# 建立景點座標（從火車站到溫泉公園，再到國小）
+trail_coordinates = [
+    [24.827020, 121.774690],  # 礁溪火車站
+    [24.829552, 121.773001],  # 湯圍溝公園
+    [24.827657, 121.772424],  # 礁溪溫泉公園
+    [24.821977, 121.768499],  # 礁溪國小
+    [24.834333303714846, 121.74706869325325] # 五峰旗瀑布
+]
+
+folium.PolyLine(
+    trail_coordinates,
+    tooltip="散步路線",
+    color="#fa0",
+    weight=6,
+).add_to(jiaoxi_map)
+
+# 建立圖層群組（FeatureGroups）
+"""
+1. 交通 traffic
+2. 教育 school
+3. 溫泉 spa
+4. 自然 nature
+"""
+group_traffic = FeatureGroup("交通").add_to(jiaoxi_map)
+group_school = FeatureGroup("教育").add_to(jiaoxi_map)
+group_spa = FeatureGroup("溫泉").add_to(jiaoxi_map)
+group_nature = FeatureGroup("自然").add_to(jiaoxi_map)
+
+# 景點資訊
+locations = {
+     "礁溪火車站": {
+        "coords": [24.827020, 121.774690],
+        "icon": ("train", "blue", "fa"),  # (icon name, color, prefix)
+        "group_map":group_traffic
+    },
+    "湯圍溝公園": {
+        "coords": [24.829552, 121.773001],
+        "icon": ("spa", "green", "fa"),
+        "group_map":group_spa
+    },
+    "礁溪溫泉公園": {
+        "coords": [24.827657, 121.772424],
+        "icon": ("hot-tub", "red", "fa"),
+        "group_map":group_spa
+    },
+    "礁溪國小": {
+        "coords": [24.821977, 121.768499],
+        "icon": ("graduation-cap", "orange", "fa"),
+        "group_map": group_school
+    },
+    "五峰旗瀑布": {
+        "coords": [24.834333303714846, 121.74706869325325],
+        "icon": ("tint", "cadetblue", "fa"),
+        "group_map":group_nature
+    },
+}
+
+# 順便加入每個景點的 marker
+for name,data in locations.items():
+    latlng = data["coords"]
+    icon_name, color, prefix = data["icon"]
+    group = data["group_map"]
+    folium.Marker(
+        location=latlng,
+        tooltip=name,
+        icon=folium.Icon(icon=icon_name,prefix=prefix,color=color)
+       ).add_to(group)
+
+# 加入圖層控制開關
+folium.LayerControl().add_to(jiaoxi_map)
+
+# 儲存地圖
+jiaoxi_map.save("C:/Users/cindy/OneDrive/桌面/Python_practise/教學影片練習/Chapter11/jiaoxi_map.html")
