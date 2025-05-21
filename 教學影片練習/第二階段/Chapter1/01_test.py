fbc49f0be4fbf0c7b22cf88a5c9eddd2824568e5
Yan <podoyo23@gmail.com>
2025-05-21 13:53:02 +0800

    創建第二階段的01

---
+++ 教學影片練習/第二階段/Chapter1/01_test.py
@@ -0,0 +1,15 @@
+# 1. 設計一個 Class
+class Student:
+    name = None
+    age = None
+    gender = None
+# 2. 創建一個對象
+stu_1 = Student()
+# 3. 對象屬性附值
+stu_1.name = "Amy"
+stu_1.gender = "Girl"
+stu_1.age = 18
+# 4. 獲取訊息
+print(stu_1.name)
+print(stu_1.gender)
+print(stu_1.age)
